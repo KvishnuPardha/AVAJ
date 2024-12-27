20c9cd0cd8b4d6c0acdb118e4b040b9eb6ef4cb2
vishnu1040 <kvp1040@gmail.com>
2024-12-26 21:42:38 -0600

    Added method sum to user.java

--- user.java
+++ user.java
@@ -1,5 +1,16 @@
 class user {
+   /**
+    * This method adds two numbers
+    * @param a
+    * @param b
+    * @return sum of a and b
+    */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World");
+        System.out.println("Addition of 2 and 3 is: " + add(2, 3));
     }
 }
