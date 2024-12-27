4cda6a979ccd71fca206e69b4ea6fb6b428d49b7
vishnu1040 <kvp1040@gmail.com>
2024-12-26 22:25:30 -0600

    bug file updated

--- bug.java
+++ bug.java
@@ -1,4 +1,10 @@
 public class bug {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Addition of 2 and 3 is: " + add(2, 3));
+        System.out.println("Subtraction of 2 and 3 is: " + sub(2, 3));
+        System.out.println("Addition of 2 and 3 is: " + add(2, 3));
+        
+    }
 }
 // Output: Compilation Error
