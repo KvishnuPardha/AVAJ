3d08307a877e6fa6bf27560ec3edb1992ef52981
vishnu1040 <kvp1040@gmail.com>
2024-12-26 22:13:51 -0600

    Error bug-sub static is resolved in 1-err-sub-met

--- user.java
+++ user.java
@@ -22,5 +22,6 @@ class user {
         System.out.println("Hello World");
         System.out.println("Addition of 2 and 3 is: " + add(2, 3));
         System.out.println("Subtraction of 2 and 3 is: " + sub(2, 3));
+        System.out.println("Addition of 2 and 3 is: " + add(2, 3));
     }
 }
