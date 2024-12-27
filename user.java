cf2b4e6d07dc7bd4c501c64858c92e5b408abd24
Kvishnupardha <vkandimalla2s@semo.edu>
2024-12-26 21:55:37 -0600

    New Bug-sub feature added

--- user.java
+++ user.java
@@ -8,9 +8,19 @@ class user {
     public static int add(int a, int b) {
         return a + b;
     }
-
+    
+    /**
+     * This method subtracts two numbers
+     * @param a
+     * @param b
+     * @return difference of a and b
+     */
+    public int sub(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Addition of 2 and 3 is: " + add(2, 3));
+        System.out.println("Subtraction of 2 and 3 is: " + sub(2, 3));
     }
 }
